fb1bcce6c1d66ce34f4db56b7b64d7a78a740768
ydmu6273 <80846320+ydmu6273@users.noreply.github.com>
2024-08-26 10:16:07 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 function guess_files {
+
     local num_files=$(ls -1 | wc -l)
     local guess=-1
     echo "Combien de fichiers se trouvent dans le répertoire actuel ?"
+    
     while [[ $guess -ne $num_files ]]; do
         read guess
         if [[ $guess -lt $num_files ]]; then
@@ -14,6 +16,7 @@ function guess_files {
             echo "Félicitations, vous avez trouvé le bon nombre de fichiers !"
         fi
     done
+
 }
 
 guess_files
